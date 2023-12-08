216c9cbbccfb700921c3264b7addc66ec6c544ad
Roisfaozi <roisfaozi55@gmail.com>
2023-12-09 06:07:10 +0700

    Feat: add authentication with guthub provider

--- next.config.js
+++ next.config.js
@@ -1,4 +1,26 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+// eslint-disable-next-line @typescript-eslint/no-var-requires
+
+const nextConfig = {
+  webpack: (config, { isServer }) => {
+    if (!isServer) {
+      config.externals.push({
+        bufferutil: "bufferutil",
+        "utf-8-validate": "utf-8-validate",
+        "supports-color": "supports-color",
+      });
+    }
+
+    return config;
+  },
+  images: {
+    remotePatterns: [
+      {
+        hostname: "avatars.githubusercontent.com",
+        protocol: "https",
+      },
+    ],
+  },
+};
 
 module.exports = nextConfig;
